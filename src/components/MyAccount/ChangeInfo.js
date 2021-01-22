5fa1025949380b8b077a23eeb88d786a4ce0b721
lamb572 <55807998+lamb572@users.noreply.github.com>
2021-01-22 16:36:37 +0000

    myacc tidied, css &  components, tick on send

---
+++ src/components/MyAccount/ChangeInfo.js
@@ -0,0 +1,96 @@
+import React, { useState} from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Table from 'react-bootstrap/Table';
+
+
+
+
+const ChangeInfo = ({setRoute, handleSubmit, user, email, setValidated}) => {
+    const [changeInfo, setChangeInfo] = useState(false);
+    const [emailExists, setEmailExists] =useState(true);
+    const [updateEmail, setUpdateEmail] = useState(user.email);
+    const [name, setName] =useState(user.name);
+    const [surname, setSurname] =useState(user.surname); 
+
+
+    if(setRoute ==='mainpage'){
+        setEmailExists(true);
+        setChangeInfo(false);
+        setUpdateEmail('');
+        setName('');
+        setSurname(''); 
+    }
+
+    
+    const saveDetails = (event) => {
+        
+        if (handleSubmit(event)){  
+            fetch('http://localhost:3001/updateuserinfo', {
+                    method: 'post',
+                    headers:{'Content-Type': 'application/json'},
+                    body: JSON.stringify({
+                        email: email,
+                        updateEmail: updateEmail,
+                        name: name,
+                        surname: surname,
+                    })
+                })
+                .then(response => response.json())
+                .then(user =>{
+                    if(user === 'email'){ 
+                        setEmailExists(false)
+                    }else{
+                        setValidated(true)
+                    }  
+                })
+        }} 
+    return(
+        <div>
+        {changeInfo
+            ?<Form.Group controlId="formChangeInfo">
+            <Button className="m-1" onClick={() => {setChangeInfo(false); setValidated('')}}>Hide Users Info</Button>
+            <Form.Group controlId="formGridEmail">
+            <Form.Label>Email</Form.Label>
+            { !emailExists
+                ?<Form.Text className="error" > Email Already Exists</Form.Text> 
+                :null
+                }
+            <Form.Control required value={updateEmail} onChange={(input) => setUpdateEmail(input.target.value)} type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group controlId="formGridName">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control required value={name} onChange={(input) => setName(input.target.value)} type="text" placeholder="First Name" />
+            </Form.Group>
+
+
+            <Form.Group controlId="formGridSurname">
+            <Form.Label>Surname</Form.Label>
+            <Form.Control required value={surname} onChange={(input) => setSurname(input.target.value)} type="text" placeholder="Surname" />
+            </Form.Group>
+            <Button className="m-1" onClick={saveDetails} variant="primary">
+                    Save Details
+            </Button>
+        </Form.Group>
+        :<Table responsive striped bordered hover>
+            <thead>
+                <tr>
+                    <th>Account Information</th>
+                </tr>
+            </thead>
+            <tbody>    
+                <tr>
+                    <td>Email: {email}<br/>
+                    First Name: {name}<br/>
+                    Surname: {surname}<br/>
+                    <Button className="mt-2" onClick={() => {setChangeInfo(true) ; setValidated('')}}>Change User Info</Button>
+                    </td>
+                </tr> 
+            </tbody>
+        </Table>
+        } 
+        </div>
+    )
+}
+export default ChangeInfo
---
+++ src/components/MyAccount/ChangePassword.js
@@ -0,0 +1,87 @@
+import React, { useState} from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+
+const ChangePassword =({setRoute, handleSubmit, setValidated, email}) =>{
+
+    const [changePassword, setChangePassword] =useState(false)
+    const [password, setPassword] =useState('111111');
+    const [confirmPassword, setConfirmPassword] =useState('111111');
+    const [currentPassword, setCurrentPassword] =useState('')
+    const [oldPassword, setOldPassword] = useState(true);
+
+    if(setRoute ==='mainpage'){
+        setChangePassword(false)
+        setPassword('');
+        setConfirmPassword('');
+        setCurrentPassword('')
+        setOldPassword(true);
+    }
+
+
+    
+
+    const savePassword = (event) => {
+        setOldPassword(true);
+        if (handleSubmit(event)){   
+            fetch('http://localhost:3001/updatepassword', {
+                method: 'post',
+                headers:{'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    email:email,
+                    password:password,
+                    currentPassword:currentPassword,
+                    confirmPassword:confirmPassword
+                })
+            })
+            .then(response => response.json())
+            .then(user =>{
+                if(user === 'wrongcredentials'){ 
+                    setOldPassword(false)
+                }else{
+                    setValidated(true)
+                }  
+            })
+    }};
+
+    return(
+        <div>
+            {changePassword
+                ?<Form.Group controlId="formChangePassword">
+                    <Button className="m-1" onClick={() => {setChangePassword(false); setValidated('')}}>Hide Password</Button>
+                    
+                    <Form.Group controlId="formGridOldPassword">
+                    <Form.Label>Current Password</Form.Label>
+                    { !oldPassword
+                        ?<Form.Text className="error" > Passwords do not match</Form.Text> 
+                        :null
+                        }
+                    <Form.Control required value={currentPassword} onChange={(input) => setCurrentPassword(input.target.value)} type="password" placeholder="Password" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formGridNewPassword">
+                    <Form.Label>New Password</Form.Label>
+                    <Form.Control required value={password} onChange={(input) => setPassword(input.target.value)} type="password" placeholder="Password" />
+                    </Form.Group>
+                    
+                    <Form.Group controlId="formGridConfirmPassword">
+                    <Form.Label>Confirm New Password</Form.Label>
+                        { password !== confirmPassword
+                        ?<Form.Text className="error" > Passwords do not match</Form.Text> 
+                        :null
+                        }
+                    <Form.Control required value={confirmPassword} onChange={(input) => setConfirmPassword(input.target.value)} type="password" placeholder="Password" />
+                    <Button onClick={savePassword} variant="primary">
+                        Save Password
+                    </Button>
+                    </Form.Group>
+                </Form.Group>
+                :<Button className="m-2" onClick={() => {setChangePassword(true); setValidated('')}}>Change Password</Button>
+                }
+        </div>
+
+    )
+}
+
+export default ChangePassword
